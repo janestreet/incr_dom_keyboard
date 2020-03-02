7e2503b78ec528602b3d4d29851341c9cd6ee8f4
Xavier Clerc <xclerc@janestreet.com>
2020-03-02 09:44:56 +0000

    v0.14-preview.122.10+266

--- src/vdom_keyboard.ml
+++ src/vdom_keyboard.ml
@@ -4,3 +4,13 @@ module Keyboard_event_handler = Keyboard_event_handler
 module Keystroke = Keystroke
 module Variable_keyboard_event_handler = Variable_keyboard_event_handler
 module Keyboard_event = Keyboard_event
+
+let with_keyboard_handler node keyboard_handler =
+  let open Virtual_dom.Vdom in
+  Node.div
+    [ Attr.on_keydown (fun event ->
+        Keyboard_event_handler.handle_event keyboard_handler event
+        |> Option.value ~default:Event.Ignore)
+    ]
+    [ node ]
+;;
